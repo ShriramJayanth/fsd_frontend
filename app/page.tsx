89c53686019a28d05d26161619b21bc1085fa6c8
ShriramJayanth <csshriramjayanth.cse2022@citchennai.net>
2024-12-17 10:13:40 +0530

    removed unused vars

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import Image from "next/image";
-import { use, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [FirstName, setFirstName] = useState("");
